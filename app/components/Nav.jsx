8528d5529866f8f3c315dfcfb66b556f0ef389c8
Daniel Newman <dwnewman78@gmail.com>
2016-05-09 14:15:39 -0500

    fixed nav style bugs

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -25,7 +25,7 @@ var Nav = React.createClass({
         </div>
         <div className="top-bar-right">
           <form onSubmit={this.onSearch}>
-            <ul class="menu">
+            <ul className="menu">
               <li><input type="search" placeholder="Search weather"/></li>
               <li><input type="submit" className="button" value="Get Weather"/></li>
             </ul>
